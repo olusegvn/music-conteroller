968ed90d90c05ac892b0d7ecdf546ca498ec1a28
olusegvn <olusegvn@gmail.com>
2021-12-07 21:46:32 +0000

    Added room creation with code generator and session manager.

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -6,15 +6,14 @@ import string
 
 def generate_code():
     len = 6
-    print(Room.objects)
+    print(Room.objects.all())
     while True:
         code = ''.join(random.choices(string.ascii_uppercase, k=len))
         if Room.objects.filter(code=code).count() == 0:
-            break
-        return code
+            return code
 
 class Room(models.Model):
-    code = models.CharField(default="", unique=True, max_length=50)
+    code = models.CharField(default=generate_code, unique=True, max_length=50)
     host = models.CharField(unique=True, max_length=50)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
